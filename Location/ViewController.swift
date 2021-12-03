c1412bdccb4b16763feaddf2432f847c20dc9316
vigneshrani <ranivignesh7@gmail.com>
2021-12-03 09:49:47 +0530

    Initial Commit

---
+++ Location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by vignesh on 03/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
